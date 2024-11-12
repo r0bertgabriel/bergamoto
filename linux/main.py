344141f935ac35f7716f83dc112883b28199473b
r0bert <robertdsgabriel@gmail.com>
2024-11-12 20:57:52 -0300

    Update file paths in create-people.py and main.py to reflect new project directory structure and remove unnecessary encoding in messagebox prompts

--- linux/main.py
+++ linux/main.py
@@ -16,15 +16,11 @@ photo_window_open = False
 lock = threading.Lock()
 
 def create_table():
-    # Define o caminho do banco de dados
     db_path = os.path.join('/home/br4b0/Desktop/novo_lar/bergamoto/data', 'bergamoto.db')
-    # Verifica se o diretório do banco de dados existe, se não, cria o diretório
     if not os.path.exists('/home/br4b0/Desktop/novo_lar/bergamoto/data'):
         os.makedirs('/home/br4b0/Desktop/novo_lar/bergamoto/data')
-    # Conecta ao banco de dados
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
-    # Cria a tabela 'horarios' se ela não existir
     c.execute('''CREATE TABLE IF NOT EXISTS horarios
                  (id INTEGER PRIMARY KEY AUTOINCREMENT,
                   name TEXT,
@@ -44,16 +40,14 @@ def insert_record(name, pin, timestamp, photo_blob, setor, supervisor):
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
     
-    # Verifica o número de registros para o usuário na data atual
     c.execute("SELECT COUNT(*) FROM horarios WHERE pin = ? AND date = ?", (pin, date))
     record_count = c.fetchone()[0]
     
     if record_count >= 4:
-        messagebox.showwarning("Limite Atingido".encode('utf-8'), "Você já fez 4 registros hoje.".encode('utf-8'))
+        messagebox.showwarning("Limite Atingido", "Você já fez 4 registros hoje.")
         conn.close()
         return False
 
-    # Insere um novo registro na tabela 'horarios'
     c.execute("INSERT INTO horarios (name, pin, date, time, photo, setor, supervisor) VALUES (?, ?, ?, ?, ?, ?, ?)", 
               (name, pin, date, time, photo_blob, setor, supervisor))
     conn.commit()
@@ -72,12 +66,12 @@ def capture_photo():
             with io.BytesIO() as output:
                 img.save(output, format="PNG")
                 img_blob = output.getvalue()
-            messagebox.showinfo("Captura de Foto".encode('utf-8'), "Foto capturada".encode('utf-8'))
+            messagebox.showinfo("Captura de Foto", "Foto capturada")
             cam.release()
             cv2.destroyAllWindows()
             root.quit()
         else:
-            messagebox.showerror("Erro".encode('utf-8'), "Falha ao capturar a imagem".encode('utf-8'))
+            messagebox.showerror("Erro", "Falha ao capturar a imagem")
 
     def show_frame():
         if not cam.isOpened() or not root.winfo_exists():
@@ -96,7 +90,7 @@ def capture_photo():
     root = ThemedTk(theme="equilux")
     root.title("Captura de Foto")
     root.attributes("-topmost", True)
-    root.attributes("-fullscreen", True)  # Define a janela para tela cheia
+    root.attributes("-fullscreen", True)
 
     style = ttk.Style(root)
     style.theme_use('equilux')
@@ -110,12 +104,11 @@ def capture_photo():
     capture_button = ttk.Button(frame, text="Capturar Foto", command=take_photo)
     capture_button.pack(pady=10)
 
-    root.bind('<Return>', take_photo)  # Vincula a tecla Enter à função take_photo
+    root.bind('<Return>', take_photo)
 
     photo_window_open = True
     show_frame()
     
-    # Tira a foto automaticamente após 3 segundos
     root.after(3000, take_photo)
     
     root.mainloop()
@@ -158,7 +151,7 @@ def main():
 
     csv_path = 'data/people.csv'
     if not os.path.exists(csv_path):
-        print(f"Arquivo {csv_path} não encontrado.".encode('utf-8'))
+        print(f"Arquivo {csv_path} não encontrado.")
         return
 
     with open(csv_path, mode='r') as file:
@@ -191,7 +184,7 @@ def main():
             root = ThemedTk(theme="equilux")
             root.title("Entrada de PIN")
             root.attributes("-topmost", True)
-            root.attributes("-fullscreen", True)  # Define a janela para tela cheia
+            root.attributes("-fullscreen", True)
 
             style = ttk.Style(root)
             style.theme_use('equilux')
@@ -199,17 +192,17 @@ def main():
             frame = ttk.Frame(root, padding="10")
             frame.pack(expand=True, fill=tk.BOTH)
 
-            label = ttk.Label(frame, text="Digite seu PIN:".encode('utf-8'), font=("Helvetica", 16))
+            label = ttk.Label(frame, text="Digite seu PIN:", font=("Helvetica", 16))
             label.pack(pady=10)
 
             pin_entry = ttk.Entry(frame, font=("Helvetica", 16), justify='center')
             pin_entry.pack(pady=10)
-            pin_entry.focus_set()  # Define o foco para o widget de entrada
+            pin_entry.focus_set()
 
-            submit_button = ttk.Button(frame, text="Enviar".encode('utf-8'), command=submit_pin)
+            submit_button = ttk.Button(frame, text="Enviar", command=submit_pin)
             submit_button.pack(pady=10)
 
-            root.bind('<Return>', lambda event: submit_pin())  # Vincula a tecla Enter à função submit_pin
+            root.bind('<Return>', lambda event: submit_pin())
 
             root.mainloop()
             root.destroy()
@@ -230,7 +223,7 @@ def main():
             root = ThemedTk(theme="equilux")
             root.title("Confirmação de Funcionário")
             root.attributes("-topmost", True)
-            root.attributes("-fullscreen", True)  # Define a janela para tela cheia
+            root.attributes("-fullscreen", True)
 
             style = ttk.Style(root)
             style.theme_use('equilux')
@@ -238,20 +231,20 @@ def main():
             frame = ttk.Frame(root, padding="10")
             frame.pack(expand=True, fill=tk.BOTH)
 
-            label = ttk.Label(frame, text=f"Nome: {employee.name}, Setor: {employee.setor}. É você?".encode('utf-8'), font=("Helvetica", 16))
+            label = ttk.Label(frame, text=f"Nome: {employee.name}, Setor: {employee.setor}. É você?", font=("Helvetica", 16))
             label.pack(pady=10)
 
             button_frame = ttk.Frame(frame)
             button_frame.pack(pady=10)
 
-            confirm_button = ttk.Button(button_frame, text="Sim".encode('utf-8'), command=confirm)
+            confirm_button = ttk.Button(button_frame, text="Sim", command=confirm)
             confirm_button.pack(side=tk.LEFT, padx=20)
 
-            cancel_button = ttk.Button(button_frame, text="Não".encode('utf-8'), command=cancel)
+            cancel_button = ttk.Button(button_frame, text="Não", command=cancel)
             cancel_button.pack(side=tk.RIGHT, padx=20)
 
-            root.bind('<Return>', lambda event: confirm())  # Vincula a tecla Enter à função confirm
-            confirm_button.bind('<Return>', lambda event: confirm())  # Garante que a tecla Enter funcione no botão de confirmação
+            root.bind('<Return>', lambda event: confirm())
+            confirm_button.bind('<Return>', lambda event: confirm())
 
             root.mainloop()
             root.destroy()
@@ -260,17 +253,17 @@ def main():
     while True:
         pin = get_pin()
         if pin == '----':
-            print("Encerrando o programa.".encode('utf-8'))
+            print("Encerrando o programa.")
             break
         if pin in employees:
             employee = employees[pin]
             if confirm_employee(employee):
                 employee.clock_in()
-                time.sleep(5)  # Aguarda a captura da foto e a inserção do registro serem concluídas
+                time.sleep(5)
             else:
-                messagebox.showerror("Erro".encode('utf-8'), "Confirmação falhou. Tente novamente.".encode('utf-8'))
+                messagebox.showerror("Erro", "Confirmação falhou. Tente novamente.")
         else:
-            messagebox.showerror("Erro".encode('utf-8'), "PIN incorreto. Tente novamente.".encode('utf-8'))
+            messagebox.showerror("Erro", "PIN incorreto. Tente novamente.")
 
 if __name__ == "__main__":
     main()
